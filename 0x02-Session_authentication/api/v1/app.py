b8c55c3f684f1599a30d84815ba4eeed1a716889
Umoh Andem <andemumoh@gmail.com>
2023-06-01 20:09:44 +0100

    Task 0

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -36,13 +36,16 @@ def before_request():
         pass
     else:
         excluded_list = ['/api/v1/status/',
-                         '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                         '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                         '/api/v1/auth_session/login/']
 
         if auth.require_auth(request.path, excluded_list):
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(request)\
+                 and not auth.session_cookie(request):
                 abort(401, description="Unauthorized")
             if auth.current_user(request) is None:
                 abort(403, description='Forbidden')
+            request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
